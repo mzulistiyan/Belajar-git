f0b92dcd42560df881b4c862cfaea2a6d0799623
izulistiyan@gmail.com <izulistiyan@gmail.com>
2023-01-18 14:16:20 +0700

    Ganti Title Ammar

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo Ammar',
+      title: 'Flutter Demo Ammarr',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
